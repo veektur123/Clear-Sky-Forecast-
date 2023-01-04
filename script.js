a32ac5203a5f25be4244fe7dabf240ebeaceca6e
VICTOR RIVERA <victorrivera@victors-air.myfiosgateway.com>
2023-01-03 23:23:27 -0500

    Set up API key to retrieve weather data for the next 5 days

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 const Apikey = "8fee0ba025c40be55108dfce2bc435df"
+const searchInput = document.getElementById("search-input")
+const searchButton = document.getElementById("search-button")
 
+searchButton.addEventListener("click", (e)=>{
+    e.preventDefault()
+    const city = searchInput.value 
+    console.log(city)
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Apikey}`
+    fetch(url).then((response)=>{
+        return response.json()
+    }).then((data)=>{
+        console.log(data)
+        const {lon,lat} = data.coord
+        const fiveDayUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${Apikey}`
+        return fetch(fiveDayUrl)
+    }).then((response)=> {
+        return response.json()
+        
+    }).then((data)=>{
+        console.log(data)
+    })
+
+    
+})
